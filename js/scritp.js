8ee78449f9e53b24dbd45396ec48d13a42b33947
Tim-Edvardsson <tim.edvardsson@elev.ga.ntig.se>
2023-03-28 14:47:58 +0200

    Sista lektionen
    Jag kommit långt

---
+++ js/scritp.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById("burger")
+const linklist = document.getElementById("link_list")
+
+function toggleMenu() {
+    linklist.classList.toggle("link_list_visible")
+    burger.classList.toggle("burger_in_x_form")
+}
+
+burger.addEventListener("click", toggleMenu)
