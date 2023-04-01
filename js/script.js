903f57acbfb302a30b4713e4366180cb45d895b3
Tim-Edvardsson-NTI-Johanneberg <tim.edvardsson@elev.ga.ntig.se>
2023-04-01 18:50:11 +0200

    Mobile shop and landing page
    Mobile shop and landing page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const burger = document.getElementById("burger")
 const linklist = document.getElementById("link_list")
-
-function toggleMenu() {
+    function toggleMenu() {
     linklist.classList.toggle("link_list_visible")
     burger.classList.toggle("burger_in_x_form")
 }
-
-burger.addEventListener("click", toggleMenu)
+    burger.addEventListener("click", toggleMenu)
